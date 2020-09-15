2de86d62a88670b7cf84624d11562f93544476d7
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-14 20:45:45 -0600

    First commit

---
+++ R/postgres_connector.R
@@ -0,0 +1,39 @@
+#' postgres_connector
+#' @importFrom RPostgres Postgres
+#' @importFrom glue glue
+#' @importFrom DBI dbConnect
+#' @export postgres_connector
+postgres_connector <- function(host = Sys.getenv('POSTGRES_HOST'),
+                               port = Sys.getenv('POSTGRES_PORT'),
+                               user = Sys.getenv('POSTGRES_USER'),
+                               password = Sys.getenv('POSTGRES_PASSWORD'),
+                               dbname = Sys.getenv('POSTGRES_DB'),
+                               max_attempts = 2,
+                               verbose = TRUE) {
+  n <- 1
+  if (verbose) message('First attempt at connection')
+  repeat {
+    connection <-
+      try({
+        dbConnect(Postgres(),
+                  host = host,
+                  port = port,
+                  user = user,
+                  password = password,
+                  dbname = dbname)
+      })
+    if (inherits(connection, 'try-error')) {
+      if (n > max_attempts) {
+        if (verbose) stop('Connection to DB failed')
+      }
+      n <- n + 1
+      if (verbose) message(glue('Trying to connect: try {n}'))
+    } else {
+      if (verbose) message(glue('Connected at {host'))
+      break
+    }
+  }
+
+  connection
+
+}
